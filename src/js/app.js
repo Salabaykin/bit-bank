c461e2fa207a0148d2c1c491a7f7b720dc477314
Evgeny <salabaikin@mail.ru>
2021-01-06 18:23:26 +0300

    Popup fix

--- src/js/app.js
+++ src/js/app.js
@@ -435,61 +435,41 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   }
 
-  // Popup 
-  const modalInit = () => {
-    const popupLink = document.querySelectorAll('.popup'),
-          modal = document.querySelectorAll('.modal'),
-          modalClose = document.querySelectorAll('.modal__close'),
-          overlay = document.querySelectorAll('.modal__overlay'),
-          marginRight = window.innerWidth - document.documentElement.clientWidth;
-
-    popupLink.forEach(link => {
-      link.addEventListener('click', (e) => {
-        e.preventDefault();
-        const target = e.target;
-
-        const attrLinkValue = target.getAttribute('data-link-id');
-
-        modal.forEach(item => {
-          const itemAttr = item.getAttribute('data-modal-id');
-          if (itemAttr === attrLinkValue) {
-            item.classList.add('modal-open');
-            document.body.classList.add('hidden');
-            if (document.documentElement.clientWidth > 991) {
-              document.body.style = `margin-right: ${marginRight}px`;
-            } else {
-              document.body.style = `margin-right: 0px`;
-            }
-          }
-          overlay.forEach(elem => {
-            elem.addEventListener('click', () => {
-              item.classList.remove('modal-open');
-              document.body.classList.remove('hidden');
-              document.body.style = `margin-right: 0px`;
-            });
-          });
+  // Modal 
+  const popUp = () => {
+    const modalData = {
+      popupLink: document.querySelectorAll('.popup'),
+      modal: document.querySelectorAll('.modal'),
+      marginSize() {
+        return window.innerWidth - document.documentElement.clientWidth;
+      },
+      closeModal(elem) {
+        const closeBtn = elem.querySelector('.modal-close');
+        closeBtn.addEventListener('click', () => {
+          elem.classList.remove('modal-open');
         });
+      },
+      openModal(attr) {
+        this.modal.forEach((elem) => {
+          const elemAttr = elem.getAttribute('data-modal-id');
+          if (attr === elemAttr) {
+            elem.classList.add('modal-open');
+            this.closeModal(elem);
+          }
+        });
+      },
+    };
+
+    modalData.popupLink.forEach(elem => {
+      elem.addEventListener('click', (event) => {
+        event.preventDefault();
+        const target = event.target;
+        const attrLinkValue = target.getAttribute('data-link-id');
+        modalData.openModal(attrLinkValue);
       });
     });
-
-    const close = () => {
-      modalClose.forEach(elem => {
-        elem.addEventListener('click', () => {
-          modal.forEach(item => {
-            if (item.classList.contains('modal-open')) {
-              item.classList.remove('modal-open');
-              document.body.classList.remove('hidden');
-              document.body.style = `margin-right: 0px`;
-            }
-          });
-        });
-      });
-    }
-
-    close();
-    
   };
 
-  modalInit();
+  popUp();
 
 });
